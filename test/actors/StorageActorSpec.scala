f58a4dea7b531f98ffe9b16fa72b3bbea76d39c5
Ilya Zinkovich <ilya.zinkovich@gmail.com>
2017-09-02 16:23:29 +0300

    Fixed failing CI build

--- test/actors/StorageActorSpec.scala
+++ test/actors/StorageActorSpec.scala
@@ -8,8 +8,14 @@ import clients.ElasticSearchClient
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 import play.api.libs.json.Json
+import akka.pattern.ask
+import play.api.Configuration
+import play.api.libs.ws.WSClient
+import play.api.test.WsTestClient
 
 import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class StorageActorSpec extends TestKit(ActorSystem("testSystem")) with ImplicitSender
   with WordSpecLike with Matchers with BeforeAndAfterAll with MockFactory {
@@ -21,17 +27,17 @@ class StorageActorSpec extends TestKit(ActorSystem("testSystem")) with ImplicitS
   private implicit val timeout: Timeout = 5.seconds
 
 
-  "A Storage actor" must {
+  "A Storage actor" should {
 
     "store data in ElasticSearch" in {
-      val elasticSearchClient = mock[ElasticSearchClient]
-      val json = Json.parse("{\"name\":\"Riya\"}")
+      WsTestClient.withClient { client =>
+        val configuration = Configuration(("elasticsearch.baseUrl", "http://localhost:9200"))
+        val elasticSearchClient = new ElasticSearchClient(client, configuration)
+        val json = Json.parse("{\"id\":\"id\",\"name\":\"Riya\"}")
 
-      (elasticSearchClient.store _).expects(json)
-
-      val storageActor = system.actorOf(StorageActor.props(elasticSearchClient))
-      storageActor ! Store(json)
+        val storageActor = system.actorOf(StorageActor.props(elasticSearchClient))
+        storageActor ? Store(json)
+      }
     }
-
   }
 }
